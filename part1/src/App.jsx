21d0f5701ca0e3609070b8cc29ec6290bc9805d4
Derik Zhu <derikzhu@dhcp-10-8-003-015.mobile.reshsg.uci.edu>
2024-10-25 22:28:46 -0700

    Part 1C

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,3 +1,5 @@
+import { useState } from 'react'
+
 const Hello = (props) => {
   console.log(props)
   return (
@@ -9,15 +11,45 @@ const Hello = (props) => {
   )
 }
 
-const App = () => {
-  const name = 'Derik'
-  const age = 18
+const Display = ({counter}) => <div>{counter}</div>
+
+const Button = (onClick, text) => <button onClick={onClick}>{text}</button>
+
+const App = (props) => {
+  const [ counter, setCounter ] = useState(0)
+  console.log('rendering with counter value', counter)
+
+  const increaseByOne = () => {
+    console.log('increasing, value before', counter)
+    setCounter(counter + 1)
+  }
+
+  const decreaseByOne = () => {
+    console.log('decreasing, value before', counter)
+    setCounter(counter - 1)
+  }
+
+  const setToZero = () => {
+    console.log('resetting to zero, value before', counter)
+    setCounter(0)
+  }
+
   return (
-    <>
-      <Hello name={name} age={age} />
-      <Hello name='Greysun' age={18} />
-      <Hello name='Euan' age={10+8} />
-    </>
+    <div>
+      <Display counter={counter} />
+      <Button 
+        onClick={increaseByOne}
+        text='plus'
+      />
+      <Button 
+        onClick={decreaseByOne}
+        text='minus'
+      />
+      <Button 
+        onClick={setToZero}
+        text='zero'
+      />
+    </div>
   )
 }
 

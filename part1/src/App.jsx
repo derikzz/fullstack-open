46a11d8e0a5455f210f3ef503710f8035374f810
Derik Zhu <derikzhu@dhcp-10-8-003-015.mobile.reshsg.uci.edu>
2024-10-28 19:13:51 -0700

    part 1d

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,54 +1,139 @@
-import { useState } from 'react'
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
 
-const Hello = (props) => {
-  console.log(props)
+// const Hello = (props) => {
+//   console.log(props)
+//   return (
+//     <div>
+//       <p>
+//         Hello {props.name}, you are {props.age} years old
+//       </p>
+//     </div>
+//   )
+// }
+
+// const Display = ({counter}) => <div>{counter}</div>
+
+// const Button = (onClick, text) => <button onClick={onClick}>{text}</button>
+
+// const App = (props) => {
+//   const [ counter, setCounter ] = useState(0)
+//   console.log('rendering with counter value', counter)
+
+//   const increaseByOne = () => {
+//     console.log('increasing, value before', counter)
+//     setCounter(counter + 1)
+//   }
+
+//   const decreaseByOne = () => {
+//     console.log('decreasing, value before', counter)
+
+//     setCounter(counter - 1)
+//   }
+
+//   const setToZero = () => {
+//     console.log('resetting to zero, value before', counter)
+//     setCounter(0)
+//   }
+
+//   return (
+//     <div>
+//       <Display counter={counter} />
+//       <Button 
+//         onClick={increaseByOne}
+//         text='plus'
+//       />
+//       <Button 
+//         onClick={decreaseByOne}
+//         text='minus'
+//       />
+//       <Button 
+//         onClick={setToZero}
+//         text='zero'
+//       />
+//     </div>
+//   )
+// }
+
+// const History = (props) => {
+//   if (props.allClicks.length === 0) {
+//     return (
+//       <div>
+//         the app is used by pressing the buttons
+//       </div>
+//     )
+//   }
+//   return (
+//     <div>
+//       button press history: {props.allClicks.join(' ')}
+//     </div>
+//   )
+// }
+
+// const Button = ({ handleClick, text }) => (
+//   <button onClick={handleClick}>
+//     {text}
+//   </button>
+// )
+
+// const App = () => {
+//   const [left, setLeft] = useState(0)
+//   const [right, setRight] = useState(0)
+
+//   const [allClicks, setAll] = useState([])
+//   const [total, setTotal] = useState(0)
+
+
+//   const handleLeftClick = () => {
+//     setAll(allClicks.concat('L'))
+//     const updatedLeft = left + 1
+//     setLeft(updatedLeft)
+//     setTotal(updatedLeft + right)
+//   }
+
+
+//   const handleRightClick = () => {
+//     setAll(allClicks.concat('R'))
+//     const updatedRight = right + 1
+//     setRight(updatedRight)
+//     setTotal(left + updatedRight)
+//   }
+
+//   return (
+//     <div>
+//       {left}
+//       <Button handleClick={handleLeftClick} text='left' />
+//       <Button handleClick={handleRightClick} text='right' />
+//       {right}
+
+//       <History allClicks={allClicks} />
+//       <p>total {total}</p>
+//     </div>
+//   )
+// }
+
+const Display = props => <div>{props.value}</div>
+
+const Button = (props) => (
+  <button onClick={props.handleClick}>
+    {props.text}
+  </button>
+)
+
+const App = () => {
+  const [value, setValue] = useState(10)  
+
+  const setToValue = (newValue) => () => {
+    console.log('value now', newValue)  // print the new value to console
+    setValue(newValue)
+  }
+  
   return (
     <div>
-      <p>
-        Hello {props.name}, you are {props.age} years old
-      </p>
-    </div>
-  )
-}
-
-const Display = ({counter}) => <div>{counter}</div>
-
-const Button = (onClick, text) => <button onClick={onClick}>{text}</button>
-
-const App = (props) => {
-  const [ counter, setCounter ] = useState(0)
-  console.log('rendering with counter value', counter)
-
-  const increaseByOne = () => {
-    console.log('increasing, value before', counter)
-    setCounter(counter + 1)
-  }
-
-  const decreaseByOne = () => {
-    console.log('decreasing, value before', counter)
-    setCounter(counter - 1)
-  }
-
-  const setToZero = () => {
-    console.log('resetting to zero, value before', counter)
-    setCounter(0)
-  }
-
-  return (
-    <div>
-      <Display counter={counter} />
-      <Button 
-        onClick={increaseByOne}
-        text='plus'
-      />
-      <Button 
-        onClick={decreaseByOne}
-        text='minus'
-      />
-      <Button 
-        onClick={setToZero}
-        text='zero'
-      />
+      <Display value={value} />
+      <Button handleClick={setToValue(1000)} text='thousand' />
+      <Button handleClick={setToValue(0)} text='reset' />
+      <Button handleClick={setToValue(value + 1)} text='increment' />
     </div>
   )
 }

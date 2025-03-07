89689459bb2698dbafb3b088f2f166c842887c2c
ApatheticLlama <76979663+ApatheticLlama@users.noreply.github.com>
2025-03-06 22:24:04 -0800

    Part 2C and exercises

--- part2/src/main.jsx
+++ part2/src/main.jsx
@@ -1,26 +1,37 @@
+import axios from 'axios'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
 import App from './App'
 
-const notes = [
-  {
-    id: 1,
-    content: 'HTML is easy',
-    important: true
-  },
-  {
-    id: 2,
-    content: 'Browser can execute only JavaScript',
-    important: false
-  },
-  {
-    id: 3,
-    content: 'GET and POST are the most important methods of HTTP protocol',
-    important: true
-  }
-]
+// const notes = [
+//   {
+//     id: 1,
+//     content: 'HTML is easy',
+//     important: true
+//   },
+//   {
+//     id: 2,
+//     content: 'Browser can execute only JavaScript',
+//     important: false
+//   },
+//   {
+//     id: 3,
+//     content: 'GET and POST are the most important methods of HTTP protocol',
+//     important: true
+//   }
+// ]
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <App notes={notes} />
-)
+// axios
+//   .get('http://localhost:3001/notes')
+//   .then(response => {
+//     const notes = response.data
+//     ReactDOM.createRoot(document.getElementById('root')).render(
+//       <App notes={notes} />
+//     )
+//   })
+
+// const promise2 = axios.get('http://localhost:3001/foobar')
+// console.log(promise2)
+
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);

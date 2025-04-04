30fd9cfa99dcb9444f394c8d42d5eeeb5f6b7bcf
ApatheticLlama <76979663+ApatheticLlama@users.noreply.github.com>
2025-04-03 22:28:21 -0700

    Part3C first section and exercise

---
+++ part3_exercises/mongo.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose')
+
+const password = process.argv[2]
+
+const url = `mongodb+srv://monster159284:${password}@fullstackopen.srxojiq.mongodb.net/phonebook?retryWrites=true&w=majority&appName=fullstackopen`
+
+mongoose.set('strictQuery', false)
+mongoose.connect(url)
+
+
+
+const personSchema = new mongoose.Schema({
+    name: String,
+    number: String
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+
+if(process.argv.length < 5) {
+    console.log('phonebook:')
+    Person.find({}).then(result => {
+        result.forEach(person => {
+            console.log(person.name, person.number)
+        })
+        mongoose.connection.close()
+    })
+} else {
+    const name = process.argv[3]
+    const number = process.argv[4]
+
+    const person = new Person({
+        name: name,
+        number: number
+    })
+
+    person.save().then(result => {
+        console.log(`added ${name} number ${number} to phonebook`)
+        mongoose.connection.close()
+    })
+}
+

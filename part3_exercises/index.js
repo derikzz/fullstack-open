a85123a37bb6ec7b5a6f26dd8cc87c5db02601cb
ApatheticLlama <76979663+ApatheticLlama@users.noreply.github.com>
2025-04-03 19:31:34 -0700

    Part 3c exercises

--- part3_exercises/index.js
+++ part3_exercises/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const morgan = require('morgan')
 const app = express()
 
+app.use(express.static('dist'))
 app.use(express.json())
 
 morgan.token('content-length', (req, res) => res.get('Content-Length'))
@@ -108,7 +109,7 @@ app.get('/info/', (request, response) => {
     )
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001 
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
